8c9a0b7990c4316bb5867ed890331ee175de5520
Roan Dayne Dino <roandaynedenaerys@gmail.com>
2023-12-14 02:32:20 +0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Scholaris Color Palette Checker`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
